f8e3319cb83d2c617dc19e2fb58b0d04c656bbf2
GladysKigen <gladys.kigen@student.moringa.com>
2023-02-26 00:39:08 +0300

    update

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES ('Mr.Chocolate',14,'M','brown','calm',1);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES ('Rowdy',10,'F','brown','calm',1);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES ('Tabitha',15,'M','white','aggressive',0);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES ('Sergeant Brown',20,'M','black','composed',1);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES ('Melissa',16,'F','brown','bold',1);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES ('Grinch',14,'M','brown','calm',1);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES ('Wendy',10,'M','grey','bold',0);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES ('Null',14,'M','brown','calm',1);
